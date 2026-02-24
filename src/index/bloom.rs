d2d95930f662be756099e975038cc9e91254776b
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2026-02-24 11:54:32 -0600

    rustfmt src/index/bloom.rs

--- src/index/bloom.rs
+++ src/index/bloom.rs
@@ -1,28 +1,27 @@
 use std::convert::TryInto;
 
-
 /// A segment is essentially a bitmap of
 /// representing values in [0, 65536).
-/// 
-/// 
+///
+///
 /// # Examples
-/// 
+///
 /// ```
 /// use faiss::index::bloom::Segment;
-/// 
+///
 /// let mut segment = Segment::empty();
 /// segment.set(8000u16);
 /// assert!(segment.get(8000u16), "should be able to fetch a value known to exist in the bitmap.");
 /// ```
 #[derive(Debug)]
 pub struct Segment {
-    data: Box<[u8; 8192]>
+    data: Box<[u8; 8192]>,
 }
 
 impl Default for Segment {
     fn default() -> Self {
         Self {
-            data: Box::new([0u8; 8192])
+            data: Box::new([0u8; 8192]),
         }
     }
 }
@@ -53,45 +52,43 @@ impl Segment {
 
     /// Get an iterator over all the positions that have been
     /// seen at least once.
-    pub fn get_setbits(&self) -> impl Iterator<Item=u16> + use<'_> {
-        (0..=65535u16)
-        .filter(move |&index| self.get(index))
+    pub fn get_setbits(&self) -> impl Iterator<Item = u16> + use<'_> {
+        (0..=65535u16).filter(move |&index| self.get(index))
     }
 }
 
-
-/// A Bloom filter like cache that 
+/// A Bloom filter like cache that
 /// divides binary vectors into segments of 16 bits
 /// and tracks whether segments at corresponding positions
 /// have been seen before.
-/// 
-/// This cache can be used to quickly reject range queries 
+///
+/// This cache can be used to quickly reject range queries
 /// of a narrow radius since just by scanning the query vector
 /// we can determine if enough segments have not been seen before
 /// causing the query to fall outside that range from any vector
 /// in a database.
-/// 
-/// 
+///
+///
 /// # Examples
-/// 
+///
 /// ```
 /// use faiss::index::bloom::BloomCache;
-/// 
+///
 /// let mut cache = BloomCache::new(4);
 /// // 4 segments imply (4 x 16 = 64) bits (or 8 bytes) per vector.
-/// 
+///
 /// let mut vectors = vec![0u8; 8 * 2];
 /// vectors[8..16].copy_from_slice(&[255u8; 8]);
-/// 
+///
 /// // Add 2 vectors into the cache.
 /// // 0x0 and 0xFFFFFFFFFFFFFFFF
 /// cache.add(&vectors);
-/// 
+///
 /// // Since the vectors are present,
 /// // a range query of distance 0 should not be rejected.
 /// assert!(!cache.should_reject(&vectors[0..8], 0));
 /// assert!(!cache.should_reject(&vectors[8..16], 0));
-/// 
+///
 /// // Far enough vectors shouldn't be rejected anyway.
 /// assert!(!cache.should_reject(&vectors[0..8], 100));
 /// ```
@@ -104,9 +101,9 @@ pub struct BloomCache {
 impl BloomCache {
     /// Create a new Cache with the given number of 16-bits wide segments.
     pub fn new(num_segments: usize) -> Self {
-        Self { 
+        Self {
             segments: (0..num_segments).map(|_| Segment::default()).collect(),
-            size: 0
+            size: 0,
         }
     }
 
@@ -141,7 +138,7 @@ impl BloomCache {
             if !segment.get(u16::from_be_bytes(chunk)) {
                 misses += 1;
                 if misses > acceptance_radius {
-                    return true
+                    return true;
                 }
             }
         }
@@ -165,22 +162,19 @@ impl BloomCache {
     }
 
     /// Get an iterator over the underlying segments.
-    pub fn iter_segments(&self) -> impl Iterator<Item=&Segment> {
+    pub fn iter_segments(&self) -> impl Iterator<Item = &Segment> {
         self.segments.iter()
     }
     /// Get a mutable iterator over the underlying segments.
-    pub fn iter_segments_mut(&mut self) -> impl Iterator<Item=&mut Segment> {
+    pub fn iter_segments_mut(&mut self) -> impl Iterator<Item = &mut Segment> {
         self.segments.iter_mut()
     }
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::{Rng, rng};
+    use rand::{rng, Rng};
 
     #[test]
     fn segment_ops() {
@@ -200,8 +194,10 @@ mod tests {
 
     #[test]
     fn bloom_cache_recall() {
-
-        let mut index = index_binary_factory(256, "BFlat").unwrap().into_flat().unwrap();
+        let mut index = index_binary_factory(256, "BFlat")
+            .unwrap()
+            .into_flat()
+            .unwrap();
         let mut cache = BloomCache::new(index.d() as usize / 16);
 
         let num_vectors: usize = 1000;
@@ -225,8 +221,10 @@ mod tests {
 
     #[test]
     fn bloom_cache_near() {
-
-        let mut index = index_binary_factory(256, "BFlat").unwrap().into_flat().unwrap();
+        let mut index = index_binary_factory(256, "BFlat")
+            .unwrap()
+            .into_flat()
+            .unwrap();
         let mut cache = BloomCache::new(index.d() as usize / 16);
 
         let num_vectors: usize = 1;
@@ -253,7 +251,5 @@ mod tests {
                 query[idx] ^= 1 << 7;
             }
         }
-
-
     }
 }
